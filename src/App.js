1e10cf5e1916e30554fe24ee6ab34b8d78ceeb7b
kimho <rlaghekd777@naver.com>
2021-06-15 03:14:39 +0900

    use axios + func component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import PropTypes from "prop-types"
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+import Movie from "./Movie";
 
-
-function Food({ fav }) {
-  return <h1> {fav}</h1>
-}
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies1, setMovies] = useState([]);
+
+
+  useEffect(() => {
+
+    async function getMovies(){
+      const {data : { data : { movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sory_by=rating");
+      setMovies(movies);
+      setIsLoading(false);
+    }
+    getMovies();
+  },[])
   return (
     <div className="App">
-      <Food fav="kimchi" something={true} />
+    <div> {isLoading ? "Loading" : movies1.map(movie =>{
+      console.log(movie);
+      return <Movie key={movie.id} id={movie.id} title={movie.title} summary={movie.summary}
+      poster={movie.medium_cover_image} year={movie.year}
+      />
+    })}</div>
+
     </div>
   );
 }
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types"
+
+
+
+const Movie = ({id, title, summary, poster, year}) =>{
+	return <h5>{title}</h5>
+}
+
+Movie.prototype = {
+	id : PropTypes.number.isRequired,
+	title : PropTypes.string.isRequired,
+	summary : PropTypes.string.isRequired,
+	poster : PropTypes.string.isRequired,
+	year : PropTypes.number.isRequired
+}
+
+export default Movie;
